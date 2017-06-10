250fe8e99cc32d60765e94d5dbbb7e9ff981a0a6
tp023988 <tp023988@gmail.com>
2017-06-10 13:56:42 +0600

    check-in from Sony Vaio_2 3

--- src/java/psa/model/PrepareImage.java
+++ src/java/psa/model/PrepareImage.java
@@ -13,20 +13,36 @@ import psa.sql.SQL_Create;
  * @author TALGAT
  */
 public class PrepareImage {
-    public void prepareImage(){
+    public void prepareImage(){        
+        
         //if Windows
-        //String pathToDirectory = "C:\\Users\\Public\\FTP_PSA";
+        String pathWindows = "C:\\Users\\Public\\FTP_PSA";
+        String pathWindows2 = "C:\\Users\\Public\\PSA_IMAGES\\";
         //if Mac
-        //String pathToDirectory = "/Users/TALGAT/PSA_CAMERA/";
+        String pathMac = "/Users/TALGAT/PSA_CAMERA/";
+        String pathMac2 = "/Users/TALGAT/PSA_IMAGES/";
         //if Ubunutu
-        String pathToDirectory = "/home/hikuser/hikvision/192.168.1.64_codes";
-        final File folder = new File(pathToDirectory);
-        listFilesForFolder(folder);        
+        String pathUbuntu = "/home/hikuser/hikvision/192.168.1.64_codes";
+        String pathUbuntu2 = "/home/hikuser/hikvision/192.168.1.64_codes";
+        String [] pathToFolder = {pathWindows, pathMac, pathUbuntu};
+        String [] pathToReserve = {pathWindows2, pathMac2, pathUbuntu2};
+        
+        for(int i=0; i<pathToFolder.length; i++){
+            final File folder = new File(pathToFolder[i]);
+            if(folder.exists()){
+                System.out.println("path "+pathToFolder[i]);
+                listFilesForFolder(folder, pathToReserve[i]);
+            }else{
+                System.out.println("non-path "+pathToFolder[i]);
+            }
+        }
+        //final File folder = new File(pathToDirectory);
+        //listFilesForFolder(folder);
     }
-    public void listFilesForFolder(final File folder) {        
+    public void listFilesForFolder(final File folder, String reserveFolder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
+                listFilesForFolder(fileEntry, reserveFolder);
             }else{
                 if(fileEntry.exists()){
                     String [] extentions = {"jpg", "png", "jpeg", "bmp"};
@@ -43,6 +59,8 @@ public class PrepareImage {
                             sqlCreate.query(imageName);
                         }
                     }
+                    fileEntry.renameTo(new File (reserveFolder+fileName));
+                    fileEntry.delete();
                 }else{
                     System.out.println("file doesn't exist");
                 }
